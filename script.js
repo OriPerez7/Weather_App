4986ee43416fdba9906c08a529aa9dab5003820d
Orian Perez <dylanperezart@gmail.com>
2024-06-11 20:49:27 +0200

    Fix type error bugs

--- script.js
+++ script.js
@@ -32,19 +32,19 @@ search.addEventListener('click', () =>{
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
-        const humidity = document.querySelector('.weather-box .humidity span');
-        const wind = document.querySelector('.weather-box .wind span');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
 
         switch(json.weather[0].main) {
             case 'Clear':
                 image.src = 'images/clear.png'
                 break;
 
-            case 'Cloud':
+            case 'Clouds':
                 image.src = 'images/cloud.png'
                 break;
 
-            case 'Mist':
+            case 'Haze':
                 image.src = 'images/mist.png'
                 break;
 
